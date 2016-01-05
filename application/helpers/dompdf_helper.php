2630bd960d3614a788cb942e163e1bb8d2b7ac6e
Sistiandy <sistiandy@artikulpi.com>
2016-01-05 22:55:54 +0700

    Perbaikan dom pdf untuk ubah ukuran kertas

--- application/helpers/dompdf_helper.php
+++ application/helpers/dompdf_helper.php
@@ -1,12 +1,12 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
-function pdf_create($html, $filename='', $stream=TRUE, $orientation = 'portrait') 
+function pdf_create($html, $filename='', $stream=TRUE, $paper = 'A4', $orientation = 'portrait') 
 {
 
     require_once("dompdf/dompdf_config.inc.php");
     
     $dompdf = new DOMPDF();
     $dompdf->load_html($html);
-    $dompdf->set_paper('A4', $orientation);
+    $dompdf->set_paper($paper, $orientation);
     $dompdf->render();
     if ($stream) {
         $dompdf->stream($filename.".pdf", array("Attachment" => 0));
