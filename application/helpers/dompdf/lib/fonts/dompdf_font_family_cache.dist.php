d4cf15cbbda890574202dadb33ef45c393e98726
Sistiandy <sistiandy@artikulpi.com>
2016-01-30 17:43:35 +0700

    Fixing stl module print pdf change font

--- application/helpers/dompdf/lib/fonts/dompdf_font_family_cache.dist.php
+++ application/helpers/dompdf/lib/fonts/dompdf_font_family_cache.dist.php
@@ -108,4 +108,10 @@
     'italic' => DOMPDF_FONT_DIR . 'DejaVuSerifCondensed-Italic',
     'normal' => DOMPDF_FONT_DIR . 'DejaVuSerifCondensed',
   ),
+ 'calibri' => array (  
+   'normal' => DOMPDF_FONT_DIR . 'calibri',  
+   'bold' => DOMPDF_FONT_DIR . 'calibrib',  
+   'italic' => DOMPDF_FONT_DIR . 'calibrii',  
+   'bold_italic' => DOMPDF_FONT_DIR . 'calibriz'  
+ ), 
 ) ?>
