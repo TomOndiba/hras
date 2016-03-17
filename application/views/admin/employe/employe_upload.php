605628e7b63605ecbd9debf44630f8aeae6af95c
Andi Dwi Saputro <>
2016-03-17 20:31:49 +0700

    Perubahan pada view employe_upload

--- application/views/admin/employe/employe_upload.php
+++ application/views/admin/employe/employe_upload.php
@@ -2,9 +2,11 @@
 	<h1><i class="fa fa-file-excel-o text-green"></i> Upload Data Karyawan</h1>
 	
 	<body>
-		<div class="well"><?php echo form_open_multipart('admin/employe');?>
+		<div class="well"><?php echo form_open_multipart('admin/Upload_file'); //default: ('admin/employe') ?>
 			<input type="file" id="file_upload" name="userfile" size="20" />
 			<br />
 			<input type="submit" value="Upload" />
 			<?php echo form_close();?>
 		</section>
+
+
